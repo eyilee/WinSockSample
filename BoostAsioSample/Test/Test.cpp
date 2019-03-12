5f07949a9faf2ecee0ae20fd88df54ed39551c88
Eyi <eyilee@protonmail.com>
2019-03-12 11:48:57 +0800

    add command book to register commands

--- BoostAsioSample/Test/Test.cpp
+++ BoostAsioSample/Test/Test.cpp
@@ -12,11 +12,11 @@
 class Stream {
 private:
 	size_t it = 0;
-	std::vector<char> pkg;
+	std::vector<char> bytes;
 
 public:
 	std::vector<char> &data() {
-		return pkg;
+		return bytes;
 	}
 
 	template<class T>
@@ -24,42 +24,85 @@ public:
 		char *pos = (char *)&t;
 		int size = sizeof(T) / sizeof(char);
 
-		pkg.insert(pkg.end(), pos, pos + sizeof(T) / sizeof(char));
+		bytes.insert(bytes.end(), pos, pos + sizeof(T) / sizeof(char));
 
 		return *this;
 	}
 
 	template<class T>
 	Stream operator >> (T &t) {
-		t = *(T *)(&*pkg.begin() + it);
+		t = *(T *)(&*bytes.begin() + it);
 		it += sizeof(T) / sizeof(char);
 		return *this;
 	}
 };
 
+class ICommand {
+public:
+	virtual UINT getId() = 0;
+	virtual void excute() = 0;
+	virtual void serialize(Stream &stream) = 0;
+	virtual void deserialize(Stream &stream) = 0;
+};
+
 template <typename T>
-class Command {
-private:
+class Command : public ICommand {
+public:
 	static UINT ID;
 
 public:
-	virtual void Serialize(Stream &stream) {
+	UINT getId() { return ID; }
+
+	virtual void excute() = 0;
+
+	virtual void serialize(Stream &stream) {
 		stream << ID;
-		OnSerialize(stream);
+		onSerialize(stream);
 	}
 
-	virtual void OnSerialize(Stream &stream) = 0;
-
-	virtual void Derialize(Stream &stream) {
+	virtual void deserialize(Stream &stream) {
 		stream >> ID;
-		OnDerialize(stream);
+		onDeserialize(stream);
 	}
 
-	virtual void OnDerialize(Stream &stream) = 0;
+	virtual void onSerialize(Stream &stream) = 0;
+	virtual void onDeserialize(Stream &stream) = 0;
 };
 
 template <typename T>
 UINT Command<T>::ID;
+
+class ICommandGenerator {
+public:
+	virtual ICommand *Create(Stream stream) = 0;
+};
+
+template <typename T>
+class CommandGenerator : public ICommandGenerator {
+public:
+	virtual ICommand *Create(Stream stream) {
+		T *command = new T();
+		command->onDeserialize(stream);
+		return command;
+	}
+};
+
+class CommandBook {
+private:
+	std::vector<ICommandGenerator *> commands;
+
+public:
+	UINT book(ICommandGenerator *command) {
+		commands.push_back(command);
+		return commands.size() - 1;
+	}
+
+	ICommand *getCommand(Stream stream) {
+		UINT ID;
+		stream >> ID;
+		return commands[ID]->Create(stream);
+	}
+};
 
 class One : public Command<One> {
 private:
@@ -67,18 +110,26 @@ private:
 	char data2;
 
 public:
+	One() {}
+
 	One(int data1, char data2)
 		: data1(data1)
 		, data2(data2)
 	{
 	}
 
-	void OnSerialize(Stream &stream) {
+	void excute() {
+		std::cout << "class: One" << std::endl;
+		std::cout << " data1: " << data1 << std::endl;
+		std::cout << " data2: " << data2 << std::endl;
+	}
+
+	void onSerialize(Stream &stream) {
 		stream << data1;
 		stream << data2;
 	}
 
-	void OnDerialize(Stream &stream) {
+	void onDeserialize(Stream &stream) {
 		stream >> data1;
 		stream >> data2;
 	}
@@ -90,18 +141,26 @@ private:
 	bool data2;
 
 public:
+	Two() {}
+
 	Two(float data1, bool data2)
 		: data1(data1)
 		, data2(data2)
 	{
 	}
 
-	void OnSerialize(Stream &stream) {
+	void excute() {
+		std::cout << "class: Two" << std::endl;
+		std::cout << " data1: " << data1 << std::endl;
+		std::cout << " data2: " << data2 << std::endl;
+	}
+
+	void onSerialize(Stream &stream) {
 		stream << data1;
 		stream << data2;
 	}
 
-	void OnDerialize(Stream &stream) {
+	void onDeserialize(Stream &stream) {
 		stream >> data1;
 		stream >> data2;
 	}
@@ -109,5 +168,34 @@ public:
 
 int main()
 {
+	CommandBook commands;
+
+	// regesiter commands
+	One::ID = commands.book(new CommandGenerator<One>());
+	Two::ID = commands.book(new CommandGenerator<Two>());
+
+	// socket stream
+	Stream stream1;
+
+	// client send message to server
+	One *one = new One(10, 'A');
+	one->serialize(stream1);
+
+	// server get message
+	ICommand *command = commands.getCommand(stream1);
+	command->excute();
+
+	// socket stream
+	Stream stream2;
+
+	// client send message to server
+	Two *two = new Two(0.123f, true);
+	two->serialize(stream2);
+
+	// server get message
+	command = commands.getCommand(stream2);
+	command->excute();
+
+	system("pause");
 	return 0;
 }
